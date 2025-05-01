16f67a01ce9fccb880f0f61493fcd439083262e9
Mohammed Zaid <mzaid@ucsd.edu>
2025-05-01 13:25:30 -0700

    resume update

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react';
 import { motion, AnimatePresence, useAnimation } from 'framer-motion';
 import './App.css';
 import profilePic from './images/pfp.jpg';
-import resumePDF from './resume.pdf';
+import resumePDF from './resumes.pdf';
 
 function App() {
  const [activeSection, setActiveSection] = useState('about');
