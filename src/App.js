8d15cbc0bc327267ccfec87c55e2a67e71a998b9
Mohammed Zaid <mzaid@ucsd.edu>
2025-06-07 19:49:22 -0700

    year changed

--- src/App.js
+++ src/App.js
@@ -197,7 +197,7 @@ function App() {
  transition={{ delay: 0.6, duration: 0.8 }}
  >
  Hi, I am Mohammed Zaid.
- I'm a 3rd-year Mathematics & Computer Science student at UC San Diego with hands-on experience in robotics, 
+ I'm a 4th-year Mathematics & Computer Science student at UC San Diego with hands-on experience in robotics, 
  machine learning, front-end development and leading high-performing teams. I thrive on solving complex problems and 
  building innovative solutions that make a difference. Welcome to my digital space, where curiosity meets code.
  </motion.p>
