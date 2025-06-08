c7cd2f156fd1a8ce277ddcbc01c291c8d833439f
Mohammed Zaid <mzaid@ucsd.edu>
2025-06-08 13:24:04 -0700

    new resumes changes

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react';
 import { motion, AnimatePresence, useAnimation } from 'framer-motion';
 import './App.css';
 import profilePic from './images/pfp.jpg';
-import resumePDF from './resumesM.pdf';
+import resumePDF from './Mainresumes.pdf';
 
 function App() {
  const [activeSection, setActiveSection] = useState('about');
