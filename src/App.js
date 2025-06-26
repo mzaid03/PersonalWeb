b1ec9185d2bcb4f080c87876864b0c6a9647a683
Mohammed Zaid <mzaid@ucsd.edu>
2025-06-26 16:20:39 -0700

    new changes

--- src/App.js
+++ src/App.js
@@ -416,7 +416,7 @@ function App() {
  <h3>Islamic Prayer Reminder Website</h3>
  <p className="tech-stack">HTML, CSS, JavaScript, Aladhan API, Open-Meteo API, Web UI/UX</p>
  <ul>
- <li>Designed and implemented a location-based Islamic prayer reminder web application that dynamically displays daily Salah times, Hijri dates, Quranic verses by integrating the Aladhan and Open-Meteo geolocation APIs; enabled both auto-location and manual ZIP/city entry for flexible user access across regions. Engineered a visually appealing and mobile-responsive interface using modern JavaScript, HTML, and CSS, with advanced features such as animated Dhikr page titles, interactive Hadith refresh, dark mode toggling, and an embedded PDF guide for learning how to pray—prioritizing both accessibility and spiritual enrichment through thoughtful design and real-time religious functionality.</li>
+ <li>Designed and implemented a location-based Islamic prayer reminder web application that dynamically displays daily Salah times, Hijri dates, Quranic verses by integrating the Aladhan and Open-Meteo geolocation APIs; enabled both auto-location and manual ZIP/city entry for flexible user access across regions. Engineered a visually appealing and mobile-responsive interface using modern JavaScript, HTML, and CSS, with advanced features such as animated Dhikr page titles, interactive Hadith refresh, dark mode toggling, and an embedded PDF guide for learning how to pray, prioritizing both accessibility and spiritual enrichment through thoughtful design and real-time religious functionality.</li>
  </ul>
  <div className="project-visual">
  <div className="binary-tree">
