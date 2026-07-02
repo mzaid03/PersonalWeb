ece908972e9e58820726bf23436e3bf3ce7f8152
Mohammed Zaid <mzaid@ucsd.edu>
2026-07-02 13:47:12 -0700

    new resume

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react';
 import { motion, AnimatePresence, useAnimation } from 'framer-motion';
 import './App.css';
 import profilePic from './images/pfp.jpg';
-import resumePDF from './Mohammed_Zaid_Resume_.pdf';
+import resumePDF from './Mohammed_Zaid_Resume.pdf';
 import ucsdImg from './images/ucsdwebpic.jpg'; 
 
 function App() {
